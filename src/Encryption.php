062900f066db334be4ad85440c6ba006c842307e
Marcus Olsson <hello@marcusolsson.me>
2019-11-13 10:12:22 +0100

    Edits for PHP < 7.2 backwards compatability.

--- src/Encryption.php
+++ src/Encryption.php
@@ -4,37 +4,78 @@ namespace Olssonm\BackupShield;
 
 use PhpZip\ZipFile;
 
-use ZipArchive;
+use \ZipArchive;
 
 class Encryption
 {
+    /**
+     * Default encryption contants
+     *
+     * @var string
+     */
     const ENCRYPTION_DEFAULT = 'default';
+
+    /**
+     * AES-128 encryption contants
+     *
+     * @var string
+     */
     const ENCRYPTION_WINZIP_AES_128 = 'aes_128';
+
+    /**
+     * AES-192 encryption contants
+     *
+     * @var string
+     */
     const ENCRYPTION_WINZIP_AES_192 = 'aes_192';
+
+    /**
+     * AES-256 encryption contants
+     *
+     * @var string
+     */
     const ENCRYPTION_WINZIP_AES_256 = 'aes_256';
 
+    /**
+     * ZipArchive encryption constants; stores as simple string for PHP < 7.2
+     * backwards compatability
+     *
+     * @var array
+     */
     private $zipArchiveOptions = [
-        'default' => ZipArchive::EM_AES_128,
-        'aes_128' => ZipArchive::EM_AES_128,
-        'aes_192' => ZipArchive::EM_AES_192,
-        'aes_256' => ZipArchive::EM_AES_256,
+        self::ENCRYPTION_DEFAULT => '257',
+        self::ENCRYPTION_WINZIP_AES_128 => '257',
+        self::ENCRYPTION_WINZIP_AES_192 => '258',
+        self::ENCRYPTION_WINZIP_AES_256 => '259',
     ];
 
+    /**
+     * ZipFile encryption constants
+     *
+     * @var array
+     */
     private $zipFileOptions = [
-        'default' => ZipFile::ENCRYPTION_METHOD_TRADITIONAL,
-        'aes_128' => ZipFile::ENCRYPTION_METHOD_WINZIP_AES_128,
-        'aes_192' => ZipFile::ENCRYPTION_METHOD_WINZIP_AES_192,
-        'aes_256' => ZipFile::ENCRYPTION_METHOD_WINZIP_AES_256,
+        self::ENCRYPTION_DEFAULT => ZipFile::ENCRYPTION_METHOD_TRADITIONAL,
+        self::ENCRYPTION_WINZIP_AES_128 => ZipFile::ENCRYPTION_METHOD_WINZIP_AES_128,
+        self::ENCRYPTION_WINZIP_AES_192 => ZipFile::ENCRYPTION_METHOD_WINZIP_AES_192,
+        self::ENCRYPTION_WINZIP_AES_256 => ZipFile::ENCRYPTION_METHOD_WINZIP_AES_256,
     ];
 
+    /**
+     * Retrive appropriate encryption constant
+     *
+     * @param  string $type
+     * @param  string $engine
+     * @return mixed
+     */
     public function getEncryptionConstant($type, $engine)
     {
-        if ($engine == 'ZipArchive' && in_array($type, $this->zipArchiveOptions)) {
+        if ($engine == 'ZipArchive' && isset($this->zipArchiveOptions[$type])) {
             return $this->zipArchiveOptions[$type];
-        } elseif ($engine == 'ZipFile' && in_array($type, $this->zipFileOptions)) {
+        } elseif ($engine == 'ZipFile' && isset($this->zipFileOptions[$type])) {
             return $this->zipFileOptions[$type];
+        } else {
+            throw new \Exception("Encryption key not set or invalid value", 1);
         }
-
-        throw new \Exception("Encryption key not set or invalid value", 1);
     }
 }
