d4fe826882edaf5bea0dc52788be78f3827236f8
Marcus Olsson <hello@marcusolsson.me>
2020-03-10 08:41:25 +0100

    Copy-tweaks.

--- src/config/backup-shield.php
+++ src/config/backup-shield.php
@@ -5,7 +5,7 @@
         'encryption' => \Olssonm\BackupShield\Encryption::ENCRYPTION_DEFAULT
 
         // Available encryption methods:
-        // \Olssonm\BackupShield\Encryption::ENCRYPTION_DEFAULT (PHP < 7.2: PKWARE/ZipCrypto, PHP >= 7.2 AES 128)
+        // \Olssonm\BackupShield\Encryption::ENCRYPTION_DEFAULT (PHP < 7.2: PKWARE/ZipCrypto, PHP >= 7.2: AES 128)
         // \Olssonm\BackupShield\Encryption::ENCRYPTION_WINZIP_AES_128 (AES 128)
         // \Olssonm\BackupShield\Encryption::ENCRYPTION_WINZIP_AES_192 (AES 192)
         // \Olssonm\BackupShield\Encryption::ENCRYPTION_WINZIP_AES_256 (AES 256)
